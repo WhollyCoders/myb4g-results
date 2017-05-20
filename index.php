7a89b40407c6bcdb5a77285e69d0abf264a294be
Michael L Parks <github@wmi-inc.com>
2017-05-20 14:08:22 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bod4God Results</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/custom.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <img src="./images/l2l.jpg" alt="Losing to Live - Logo">
+      </header>
+      <section>
+        <div class="section-header">
+          <h1>Weekly Statistics From Week Ending March 9th</h1>
+          <h2>Our total weight loss from last week is +1.4 pounds!!!</h2>
+          <h2>Our overall total weight loss for the competition is -87.3 pounds</h2>
+        </div>
+        <hr>
+        <h2>Team Names</h2>
+        <ul>
+          <li>Cabbage Crashers</li>
+          <li>Squash Smashers</li>
+        </ul>
+        <h3>Weekly Individual Weight Loss</h3>
+        <ol>
+          <li>Competitor #1 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #2 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #3 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+        </ol>
+        <h3>Overall Individual Weight Loss</h3>
+        <ol>
+          <li>Competitor #1 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #2 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #3 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+        </ol>
+        <h3>Weekly Team Weight Loss</h3>
+        <ol>
+          <li>Competitor #1 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #2 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #3 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+        </ol>
+        <h3>Overall Team Weight Loss</h3>
+        <ol>
+          <li>Competitor #1 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #2 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+          <li>Competitor #3 - Squash Smashers (-1.6 LBS) -0.6639%</li>
+        </ol>
+        <h1>Overall Biggest Loser: Joseph Blowe (-13.6 LBS) -6.1818%</h1>
+        <h3>Most Raw Pounds Lost</h3>
+        <ol>
+          <li>Competitor #1 - (-1.6 LBS)</li>
+          <li>Competitor #2 - (-1.6 LBS)</li>
+          <li>Competitor #3 - (-1.6 LBS)</li>
+        </ol>
+        <table class="table">
+          <tr>
+            <th>Place</th>
+            <th>Team Name</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Raw LBS</th>
+            <th>% Loss</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td>Squash Smashers</td>
+            <td>Joseph</td>
+            <td>Blowe</td>
+            <td>-13.6</td>
+            <td>6.1818</td>
+          </tr>
+        </table>
+        <hr>
+        <div class="b4g-assignment">
+          <h2>Weekly Assignment</h2>
+          <h3>Your reading assignment for March 23rd is:</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="b4g-weigh-in">
+          <h2>Weigh-In Procedure</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="b4g-notes">
+          <h2>Notes To Remember</h2>
+          <ul>
+            <li>Note #1</li>
+            <li>Note #2</li>
+            <li>Note #3</li>
+            <li>Note #4</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
